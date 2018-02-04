335393db05324569c56dae00af9e0e4d389d2329
naomimachado21 <naomimachado21@gmail.com>
2018-02-04 15:18:53 -0500

    added tests + minor corrections

--- lib/calc.ex
+++ lib/calc.ex
@@ -21,13 +21,13 @@ defmodule Calc do
   @add_sub_expression_regex ~r/(\d+)(\+)(\-)(\d+)/
   @sign_regex ~r/(\+)(\-)(\d+)/
   @sign_regex2 ~r/(\-)(\+)(\d+)/
+  @sign_regex3 ~r/(\+)(\d+)/
 
   #main function
   def main() do
     exp = IO.gets(">")
     exp1 = "(#{exp})"
-    exp2 = String.replace(exp1," ","")
-    answer = eval(exp2)
+    answer = eval(exp1)
     IO.puts("#{answer}")
     main()
   end
@@ -35,6 +35,7 @@ defmodule Calc do
   #evaluate expression
   def eval(eq) do
     eq
+    |>String.replace(" ","")
     |>neg_num_subtraction
     |>neg_num_pos_num_expression
     |>mul_div_neg_num
@@ -45,6 +46,7 @@ defmodule Calc do
     |>add_sub_expression
     |>final_sign
     |>final_sign2
+    |>final_sign3
   end
 
   #handling cases like -5-9
@@ -185,6 +187,18 @@ defmodule Calc do
       if Regex.match?(@sign_regex2, chunk) do
         str8 = Regex.replace(@sign_regex2, chunk, fn _, _, _, r ->
           "-#{compute({to_int("0"), "+" , to_int(r)})}"
+        end)
+        eval(str8)
+      else
+          chunk
+      end
+  end
+
+  #handling cases like +6
+    def final_sign3(chunk) do
+      if Regex.match?(@sign_regex3, chunk) do
+        str8 = Regex.replace(@sign_regex3, chunk, fn _, _, r ->
+          "#{compute({to_int("0"), "+" , to_int(r)})}"
         end)
         eval(str8)
       else
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,7 +2,40 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
+  test "testing simple expressions" do
+    assert Calc.eval("( 5 + 6 )") == "11"
+    assert Calc.eval("( 5 - 6 )") == "-1"
+    assert Calc.eval("( 5 * 6 )") == "30"
+    assert Calc.eval("( 5 / 6 )") == "0"
+  end
+
+  test "chain expressions" do
+    assert Calc.eval("( 5 + 6 + 8)") == "19"
+    assert Calc.eval("( 10 - 2 - 3 )") == "5"
+    assert Calc.eval("( 5 * 6 * 6 )") == "180"
+    assert Calc.eval("( 100 / 5 / 2 )") == "10"
+  end
+
+  test "mix expressions" do
+    assert Calc.eval("( 5 + 6 * 4)") == "29"
+    assert Calc.eval("( 5 + 6 -8 )") == "3"
+    assert Calc.eval("( 5 * 2 / 2 )") == "5"
+    assert Calc.eval("( 4 - 2 * 2 )") == "0"
+  end
+
+  test "bdmas" do
+    assert Calc.eval("( (5 + 6) * (7 + 8) )") == "165"
+    assert Calc.eval("( 1 * (2 -(5 + 8)))") == "-11"
+    assert Calc.eval("( 24 / 6 + (5 - 4))") == "5"
+    assert Calc.eval("( 1 + 3 * 3 + 1)") == "11"
+  end
+
+  test "odd expressions" do
+    assert Calc.eval("( - 5 - 6 )") == "-11"
+    assert Calc.eval("( 5 + -6 )") == "-1"
+    assert Calc.eval("( -5 - -6 )") == "1"
+    assert Calc.eval("( -5 + 6 )") == "1"
+    assert Calc.eval("( -5 * -6 )") == "30"
+    assert Calc.eval("( -10 / -5 )") == "2"
   end
 end
