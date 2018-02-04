7296e1d8bbdb82936da8718590d2788dc375ed61
naomimachado21 <naomimachado21@gmail.com>
2018-02-04 14:48:48 -0500

    added comments

--- lib/calc.ex
+++ lib/calc.ex
@@ -22,6 +22,7 @@ defmodule Calc do
   @sign_regex ~r/(\+)(\-)(\d+)/
   @sign_regex2 ~r/(\-)(\+)(\d+)/
 
+  #main function
   def main() do
     exp = IO.gets(">")
     exp1 = "(#{exp})"
@@ -31,6 +32,7 @@ defmodule Calc do
     main()
   end
 
+  #evaluate expression
   def eval(eq) do
     eq
     |>neg_num_subtraction
@@ -45,6 +47,7 @@ defmodule Calc do
     |>final_sign2
   end
 
+  #handling cases like -5-9
   def neg_num_subtraction(chunk) do
     if Regex.match?(@neg_num_subtraction_regex, chunk) do
       str = Regex.replace(@neg_num_subtraction_regex, chunk, fn _, _, l, _, r ->
@@ -56,6 +59,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like -5+9
   def neg_num_pos_num_expression(chunk) do
     if Regex.match?(@neg_num_pos_num_expression_regex, chunk) do
       str6 = Regex.replace(@neg_num_pos_num_expression_regex, chunk, fn _, _, l, _, r ->
@@ -67,6 +71,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like -4*-9 or -5/-3
   def mul_div_neg_num(chunk) do
     if Regex.match?(@mul_div_neg_num_regex, chunk) do
       str2 = Regex.replace(@mul_div_neg_num_regex, chunk, fn _, _, l,op, _, r ->
@@ -78,6 +83,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like -5--9
   def subtraction_two_neg(chunk) do
     if Regex.match?(@subtraction_two_neg_num_regex, chunk) do
       str5 = Regex.replace(@subtraction_two_neg_num_regex, chunk, fn _, _, l, _, _, r ->
@@ -89,6 +95,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like 5--9
   def subtraction_neg_neg(chunk) do
     if Regex.match?(@subtraction_neg_neg_num_regex, chunk) do
       str5 = Regex.replace(@subtraction_neg_neg_num_regex, chunk, fn _, l, _, _, r ->
@@ -100,15 +107,25 @@ defmodule Calc do
     end
   end
 
+  #solving according bdmas
   def calculate_bdmas(equation) do
     solve(equation, :match)
   end
 
+  #checking inner parens
   def solve(equation, :no_match), do: equation
   def solve(equation, :match) do
     solve(compute_inner(equation), match_found?(@brackets_regex, equation))
   end
 
+  #finding inner parens
+  def compute_inner(equation) do
+    Regex.replace(@brackets_regex, equation, fn _, chunk ->
+      bdmas(chunk, :start)
+    end)
+  end
+
+  #solving according to bmas
   def bdmas(chunk, :done), do: chunk
   def bdmas(chunk, :start), do: bdmas(chunk, "*", :continue)
   def bdmas(chunk, op, :continue) do
@@ -120,18 +137,14 @@ defmodule Calc do
   def bdmas(chunk, "+", :no_match), do: bdmas(chunk, "-", :continue)
   def bdmas(chunk, "-", :no_match), do: bdmas(chunk, :done)
 
-  def compute_inner(equation) do
-    Regex.replace(@brackets_regex, equation, fn _, chunk ->
-      bdmas(chunk, :start)
-    end)
-  end
-
+  #calculating the answer inside parens
   def compute_answer(chunk, op) do
     Regex.replace(@positive_regexes[String.to_atom(op)], chunk, fn _, l, op, r ->
       "#{compute({to_int(l), op, to_int(r)})}"
     end)
   end
 
+  #handling cases like 5*-9 or 8/-6
   def mul_div_pos_neg(chunk) do
     if Regex.match?(@mul_div_pos_neg_regex, chunk) do
       str4 = Regex.replace(@mul_div_pos_neg_regex, chunk, fn _, l, op1, _, r ->
@@ -143,6 +156,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like 5+-9
   def add_sub_expression(chunk) do
     if Regex.match?(@add_sub_expression_regex, chunk) do
       str3 = Regex.replace(@add_sub_expression_regex, chunk, fn _, l, _, _, r ->
@@ -154,6 +168,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like +-9
   def final_sign(chunk) do
     if Regex.match?(@sign_regex, chunk) do
       str8 = Regex.replace(@sign_regex, chunk, fn _, _, _, r ->
@@ -165,6 +180,7 @@ defmodule Calc do
     end
   end
 
+  #handling cases like -+6
     def final_sign2(chunk) do
       if Regex.match?(@sign_regex2, chunk) do
         str8 = Regex.replace(@sign_regex2, chunk, fn _, _, _, r ->
@@ -176,6 +192,7 @@ defmodule Calc do
       end
   end
 
+  #finding matching patterns in the equation
   def match_found?(regex, chunk) do
     case Regex.match?(regex, chunk) do
       true -> :match
